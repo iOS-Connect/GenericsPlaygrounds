a28d7636020468a1d7141979b8a146a0bcbfb1d0
Carlos Alcala <carlos.alcala@icloud.com>
2016-10-30 15:28:29 -0700

    Fixed With Structs

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -2,48 +2,57 @@ import UIKit
 
 protocol Configurable {
     associatedtype U
-    func configure(with data:U)
+    mutating func configure(with data:U)
+}
+
+struct Element : Configurable {
+    typealias U = String
+    
+    var name: String?
+    
+    internal mutating func configure(with data: String) {
+        name = data
+    }
 }
 
 class ArrayVC<Cell: UITableViewCell> : UITableViewController where Cell:Configurable {
-    var data:[Configurable.associatedtype] = []
-
+    
+    var data = [Element]()
+    
     let cellID = "cell"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(Cell.self, forCellReuseIdentifier: cellID)
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! Cell
         cell.configure(with: data[indexPath.row])
         return cell
     }
-
+    
 }
 
 extension UITableViewCell: Configurable {
-    typealias U = String
-
-    func configure(with data: UITableViewCell.U) {
-        textLabel?.text = data
+    typealias U = Element
+    
+    func configure(with data: Element) {
+        textLabel?.text = data.name
     }
 }
 
-
 class MyVC : ArrayVC<UITableViewCell> {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        data = ["John", "Ben", "Alice"]
+        data = [Element(name: "John"), Element(name: "Ben"), Element(name: "Alice")]
         title = "Array View Controller"
     }
-
 }
 
 import PlaygroundSupport
